9c5fb9573df5d06bb25ec9582c9de2b36d5d0bf1
Piper Inns Hall <piper.innshall@gmail.com>
2026-01-11 00:02:16 +1300

    chore: Read files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::env; 
+use std::process;
+use std::fs;
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        len if len > 2 => {
+            println!("Usage: jx [script]");
+            process::exit(64);
+        }
+        2 => match run_file(&args[1]) {
+            Ok(_) => process::exit(0),
+            Err(err) => print_err(err)
+        } ,
+        _ => println!(),
+    };
+}
+
+fn print_err(err: Box<dyn Error>) {
+    println!("ERROR: \n{err}");
+    process::exit(1);
+}
+
+fn run_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let input: String = fs::read_to_string(path)?;
+    println!("{}", input);
+    Ok(())
 }
